eafcc576d40d8704adb23a5986b9757e9c2b82eb
Filippi Lorenzo <lorenzo.filippi@beta80group.it>
2019-07-04 09:33:03 +0200

    Profilo Docente

--- frontend/src/components/ProfiloDocente.js
+++ frontend/src/components/ProfiloDocente.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import {Card, Row, Col} from 'react-bootstrap'
-import '../css/ListaDocenti.css'
+import {Card, Row, Col, ListGroup} from 'react-bootstrap'
+import '../css/ProfiloDocente.css'
 import axios from 'axios'
 
 const url = document.location.href
 const stringaRicerca = url.split("/")[5]
+
+let active = 0
+let active0 = true
+let active1 = false
+let active2 = false
+let active3 = false
 
 class ProfiloDocente extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            docenti: [],
-            immagine: false
+            docenti: {},
+            immagine: false,
         }
     }
 
@@ -25,11 +31,10 @@ class ProfiloDocente extends React.Component {
             })
             .then(result => {
                 this.setState(() => ({ docenti: result }))
-             // console.log(result)   
             })
             
 
-        await axios.get(process.env.REACT_APP_URL_SERVER + 'foto_docente/' + this.state.docenti[0].cod_docente)
+        await axios.get(process.env.REACT_APP_URL_SERVER + 'foto_docente/' + stringaRicerca)
         .then(res => {
             console.log(res)
             if (res.data.base64Foto === null){ 
@@ -47,43 +52,114 @@ class ProfiloDocente extends React.Component {
         this.forceUpdate()
     }
 
+    profile() {
+        active = 0
+    }
+
+    ins() {
+        active = 1
+    }
+
+    ric() {
+        active = 2
+    }
+
+    avv() {
+        active = 3
+    }
+
     render() {
 
-        let Docenti = this.state.docenti
         let docente = []
+        
+        for ( let i = 0 ; i < this.state.docenti.length; i++) {
 
-        for ( let i = 0 ; i < Docenti.length; i++) {
-
+            let email = `mailto:${this.state.docenti[i]}`
             docente[i] = (
                 <Card key={i}>
                     <Row>
-                        <Col sm={2.5} md={2.5} lg={2.5}>
+                        <Col sm={3} md={3} lg={3}>
                             <img className="fotoProfilo" src={this.state.docenti[i].cod_foto} alt=""/>
                         </Col>
-                        <Col>
+                        <Col sm={6} md={6} lg={6}>
                             <Card.Body>
                                 <Card.Title>
-                                    <h3>{Docenti[i].nom_docente + " " + Docenti[i].cog_docente}</h3>
-                                    <h4>{Docenti[i].incarico_dir}</h4>
-                                    <h6>{"Facoltà: " + Docenti[i].des_facolta + " - " + Docenti[i].des_ssd}</h6>
-                                    <h6>{"Sede: " + Docenti[i].des_sede}</h6>
-                                    <h6>{"Ruolo: " + Docenti[i].des_ruolo}</h6>
-                                    <h6>{"Dipartimento: " + Docenti[i].des_struttura_aff.toUpperCase()}</h6>
+                                    <h3><b>{this.state.docenti[i].nom_docente + " " + this.state.docenti[i].cog_docente}</b></h3>
+                                    <br/>
+                                    {this.state.docenti[i].incarico_dir}
+                                    <br/><br/>
+                                    <h6>{"Facoltà: " + this.state.docenti[i].des_facolta + " - " + this.state.docenti[i].des_ssd}</h6>
+                                    <h6>{"Sede: " + this.state.docenti[i].des_sede}</h6>
+                                    <h6>{"Ruolo: " + this.state.docenti[i].des_ruolo}</h6>
+                                    <h6>{"Dipartimento: " + this.state.docenti[i].des_struttura_aff.toUpperCase()}</h6>
+                                </Card.Title>
+                            </Card.Body>  
+                        </Col>
+                        <Col id ="contatti" sm={3} md={3} lg={3}>
+                            <Card.Body>
+                                <Card.Title>
+                                    <h3><b>Contatti</b></h3>
                                 </Card.Title>
                                 <Card.Text>
-                                    {"Email: " + Docenti[i].email}
+                                    <br/><br/>
+                                    <img id="icons" src={require('../assets/phone-icon.png')} alt=""/>&nbsp;&nbsp;&nbsp;
+                                    <br/><br/>
+                        
+                                    <img id="icons" src={require('../assets/mail-icon.png')} alt=""/>&nbsp;&nbsp;&nbsp;<a href={email}>{this.state.docenti[i].email}</a>
                                 </Card.Text>
-                            </Card.Body>  
+                            </Card.Body>
                         </Col>
                     </Row>
                 </Card>
             )
+        }
+        
+        switch (active) {
+            case 0:
+                active0 = true
+                active1 = false
+                active2 = false
+                active3 = false
+                break;
+            case 1:
+                active0 = false
+                active1 = true
+                active2 = false
+                active3 = false
+                break;
+            case 2:
+                active0 = false
+                active1 = false
+                active2 = true
+                active3 = false
+                break;
+            case 3:
+                active0 = false
+                active1 = false
+                active2 = false
+                active3 = true
+                break;
+            default:
+                break;
         }
 
         if (this.state.immagine) {
             return (
             <div className="pagRicerca">
                 {docente}
+                <Row id="profilo">
+                    <Col id="sezioni" sm={2} md={2} lg={2}>
+                        <ListGroup variant="flush">
+                            <a href="#"><ListGroup.Item onClick={this.profile} ref="profile" value="0" active={active0}><h4>PROFILO</h4></ListGroup.Item></a>
+                            <a href="#"><ListGroup.Item onClick={this.ins} ref="ins" value="1" active={active1}><h4>INSEGNAMENTI</h4></ListGroup.Item></a>
+                            <a href="#"><ListGroup.Item onClick={this.ric} ref="ric" value="2" active={active2}><h4>RICEVIMENTO</h4></ListGroup.Item></a>
+                            <a href="#"><ListGroup.Item onClick={this.avv} ref="avv" value="3" active={active3}><h4>AVVISI</h4></ListGroup.Item></a>
+                        </ListGroup>
+                    </Col>
+                    <Col id="contenuto" sm={10} md={10} lg={10}>
+                    
+                    </Col>
+                </Row>
             </div>  
             )
         } else {
